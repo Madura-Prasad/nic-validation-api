ed53b39bd4ec5fff9f25f87e714bacabecf169ce
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-11 10:49:07 +0530

    remove node

--- Front-End/src/pages/Home.js
+++ Front-End/src/pages/Home.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     const [users, setUsers] = useState([]);
 
 
-    const {id} = useParams();
+    //const {id} = useParams();
 
     useEffect(() => {
         loadUsers();
