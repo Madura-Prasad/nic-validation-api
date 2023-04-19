bab20e48d5a0c85a4dab8eb604e126c316a9cd58
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-19 16:30:29 +0530

    replace birthday calculation userController to userService

--- Back-End/src/main/java/com/example/demo/service/UserService.java
+++ Back-End/src/main/java/com/example/demo/service/UserService.java
@@ -3,6 +3,9 @@ package com.example.demo.service;
 import java.util.List;
 
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +29,41 @@ public class UserService {
 	}
 	
 	public User saveuser(User user) {
+		
+		String nicNumber = user.getNic();
+
+		// extract the birth year, days, and gender code from the NIC number
+		int birthYear = 0;
+		int days = 0;
+		int genderCode = 0;
+		if (nicNumber.length() == 10) {
+			birthYear = 1900 + Integer.parseInt(nicNumber.substring(0, 2));
+			days = Integer.parseInt(nicNumber.substring(2, 5));
+			genderCode = Integer.parseInt(nicNumber.substring(2,5));
+		} else if (nicNumber.length() == 12) {
+			birthYear = Integer.parseInt(nicNumber.substring(0, 4));
+			days = Integer.parseInt(nicNumber.substring(4, 7));
+			genderCode = Integer.parseInt(nicNumber.substring(4,7));
+		} else {
+			throw new IllegalArgumentException("Invalid NIC number: " + nicNumber);
+		}
+
+		// calculate the birth date based on the days code
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyDDD");
+		LocalDate birthDate = LocalDate.parse(String.format("%04d%03d", birthYear, days), formatter);
+
+		// calculate the age
+		int age = Period.between(birthDate, LocalDate.now()).getYears();
+
+		// determine the gender
+		String gender = genderCode > 500 ? "Female" : "Male";
+
+		// set the birthday, age, and gender on the NIC object
+		user.setBirthday(birthDate);
+		user.setAge(age);
+		user.setGender(gender);
+		
+		
 		return userRepo.save(user);
 	}
 	
