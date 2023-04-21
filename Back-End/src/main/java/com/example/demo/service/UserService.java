1d9bb2f0c80a6998599e30a8808724ba22b55afa
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-04-21 18:34:26 +0530

    fix nic save error

--- Back-End/src/main/java/com/example/demo/service/UserService.java
+++ Back-End/src/main/java/com/example/demo/service/UserService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 import java.util.List;
 
-
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
@@ -17,39 +16,32 @@ public class UserService {
 
 	@Autowired
 	private UserRepo userRepo;
-	
-	
-	public List<User> getAll(){
+
+	public List<User> getAll() {
 		return userRepo.findAll();
 	}
-	
+
 	public User getUserById(Long id) {
-		return userRepo.findById(id).orElseThrow(()->new 
-				RuntimeException("User not Found."));
+		return userRepo.findById(id).orElseThrow(() -> new RuntimeException("User not Found."));
 	}
-	
+
 	public User saveuser(User user) {
-		
-		String nicNumber = user.getNic();
+
+		/*String nicNumber = user.getNic();
 
 		// extract the birth year, days, and gender code from the NIC number
 		int birthYear = 0;
 		int days = 0;
 		int genderCode = 0;
-		
-		var oldnic= Integer.parseInt( nicNumber.substring(2, 5));
-		var newnic = Integer.parseInt( nicNumber.substring(4, 7));
-		
-		
-		if (nicNumber.length() == 10 && (oldnic <= 366 || (oldnic >= 501 && oldnic <= 866))) {
+
+		if (nicNumber.length() == 10 && (Integer.parseInt(nicNumber.substring(2, 5)) <= 366 || (Integer.parseInt(nicNumber.substring(2, 5)) >= 501 && Integer.parseInt(nicNumber.substring(2, 5)) <= 866))) {
 			birthYear = 1900 + Integer.parseInt(nicNumber.substring(0, 2));
 			days = Integer.parseInt(nicNumber.substring(2, 5));
-			genderCode = Integer.parseInt(nicNumber.substring(2,5));
-			
-		} else if (nicNumber.length() == 12 && (newnic <= 366 || (newnic >= 501 && newnic <= 866))) {
+			genderCode = Integer.parseInt(nicNumber.substring(2, 5));
+		} else if (nicNumber.length() == 12 && (Integer.parseInt(nicNumber.substring(4, 7)) <= 366 || (Integer.parseInt(nicNumber.substring(4, 7)) >= 501 && Integer.parseInt(nicNumber.substring(4, 7)) <= 866))) {
 			birthYear = Integer.parseInt(nicNumber.substring(0, 4));
 			days = Integer.parseInt(nicNumber.substring(4, 7));
-			genderCode = Integer.parseInt(nicNumber.substring(4,7));
+			genderCode = Integer.parseInt(nicNumber.substring(4, 7));
 		} else {
 			throw new IllegalArgumentException("Invalid NIC number: " + nicNumber);
 		}
@@ -67,26 +59,70 @@ public class UserService {
 		// set the birthday, age, and gender on the NIC object
 		user.setBirthday(birthDate);
 		user.setAge(age);
+		user.setGender(gender);*/
+		
+		
+		
+		String nicNumber = user.getNic();
+
+		// extract the birth year, days, and gender code from the NIC number
+		int birthYear = 0;
+		int days = 0;
+		int genderCode = 0;
+
+		var oldnic= Integer.parseInt( nicNumber.substring(2, 5));
+		var newnic = Integer.parseInt( nicNumber.substring(4, 7));
+
+		if (nicNumber.length() == 10 && (oldnic <= 366 || (oldnic >= 501 && oldnic <= 866))) {
+		    birthYear = 1900 + Integer.parseInt(nicNumber.substring(0, 2));
+		    days = Integer.parseInt(nicNumber.substring(2, 5));
+		    genderCode = Integer.parseInt(nicNumber.substring(2, 5));
+		} else if (nicNumber.length() == 12 && (newnic <= 366 || (newnic >= 501 && newnic <= 866))) {
+		    birthYear = Integer.parseInt(nicNumber.substring(0, 4));
+		    days = Integer.parseInt(nicNumber.substring(4, 7));
+		    genderCode = Integer.parseInt(nicNumber.substring(4, 7));
+		} else {
+		    throw new IllegalArgumentException("Invalid NIC number: " + nicNumber);
+		}
+
+		// adjust the day value based on the birth year and the type of NIC number
+		if (days > 500 && days < 1000) {
+		    days -= 500;
+		}
+		
+
+		// calculate the birth date based on the days code
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyDDD");
+		LocalDate birthDate = LocalDate.parse(String.format("%04d%03d", birthYear, days), formatter);
+
+		// calculate the age
+		int age = Period.between(birthDate, LocalDate.now()).getYears();
+
+		// determine the gender
+		String gender = genderCode > 500 ? "Female" : "Male";
+
+		// set the birthday, age, and gender on the NIC object
+		user.setBirthday(birthDate);
+		user.setAge(age);
 		user.setGender(gender);
-		
-		
+
 		return userRepo.save(user);
+
+
+		
 	}
-	
-	public User updateUser(Long id,User user) {
-		User user2= userRepo.findById(id).orElseThrow(()->new 
-				RuntimeException("User not Found."));
+
+	public User updateUser(Long id, User user) {
+		User user2 = userRepo.findById(id).orElseThrow(() -> new RuntimeException("User not Found."));
 		user2.setFull_name(user.getFull_name());
 		user2.setAddress(user.getAddress());
 		user2.setMobile(user.getMobile());
 		user2.setNationality(user.getNationality());
 		return userRepo.save(user2);
 	}
-	
+
 	public void deleteUser(Long id) {
 		userRepo.deleteById(id);
 	}
-	
-	
-	
+
 }
